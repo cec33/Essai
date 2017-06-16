785c337aeaae2ec8df59854f80f1fc397d485d33
cec33 <contact@emaginetapub.com>
2017-06-16 20:00:33 +0200

    création fichier index.php

---
+++ index.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Structure Flexbox simple</title>
+  <link rel="stylesheet" href="style.css">
+  <script src="script.js"></script>
+</head>
+<body>
+  ...
+  <!-- Le reste du contenu -->
+  ...
+</body>
+</html>
+
+<!doctype html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <title>titre</title>
+        <link rel="stylesheet" href="style.css">
+        <script src="script.js"></script>
+    </head>
+    <body>
+        <header>
+            <nav>
+            </nav>
+        </header>    
+        
+        <section>
+        </section>
+        
+        <section>
+        </section>
+        
+        <footer>
+        </footer>
+    
+    </body>
+
+</html>
